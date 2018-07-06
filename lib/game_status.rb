99ea099b3764cae164f1b076e147bce24190cab9
Tao <werewaswas@live.com>
2018-07-06 06:45:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,13 +6,11 @@ end
 # Define your WIN_COMBINATIONS constant
 
 def won?(board)
-
   for each combination in WIN_COMBINATIONS do
     position_1 = board[combination[0]]
     position_2 = board[combination[1]]
     position_3 = board[combination[2]]
 
-    
     if position_1 == 'X' && position_2 == 'X' && position_3 == 'X'
       return combination
     elsif position_1 == 'O' && position_2 == 'O' && position_3 == 'O'
