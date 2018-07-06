9af4c7351566b52d261d17110c60c66bc3d96d6c
Tao <werewaswas@live.com>
2018-07-06 06:35:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,11 @@ end
 
 def won?(board)
 
-  
+  for each combination in WIN_COMBINATIONS do
+
+  end
+  position_1 = board[combination[0]]
+
   if position_1 == 'X' && position_2 == 'X' && position_3 == 'X'
     combination
   elsif position_1 == 'O' && position_2 == 'O' && position_3 == 'O'
