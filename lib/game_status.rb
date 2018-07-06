20a7ef49827faf525293e3ab5cc5ca8e0e80f723
Tao <werewaswas@live.com>
2018-07-06 06:32:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,11 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+
+def won?(board)
+  if position_1 == 'X' && position_2 == 'X' && position_3 == 'X' 
+  elsif position_1 == 'O' && position_2 == 'O' && position_3 == 'O' 
+  end
+    
+end
